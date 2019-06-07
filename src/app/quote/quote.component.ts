e745845c7aabcdf434fda93598664a81734688bf
muhindokiro <49603911+muhindokiro@users.noreply.github.com>
2019-06-07 10:45:33 +0300

    add functionality to add quote

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -21,6 +21,7 @@ export class QuoteComponent implements OnInit {
         }
           }
           }
+  
           addNewQuote(quote){
             let quoteLength = this.quotes.length;
             quote.id=quoteLength+1;
