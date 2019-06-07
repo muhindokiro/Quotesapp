f680c93d677b7e773204bfbb968ef375f1271d79
muhindokiro <blaize1143@gmail.com>
2019-06-07 10:27:56 +0300

    Auto-generated commit

--- src/app/quote.ts
+++
@@ -1,7 +0,0 @@
-export class Quote {
-    public showQuote:boolean
-    constructor(public name:string,public quote:string, public author:string, public completeDate:Date){
-        this.showQuote=false
-
-    }
-}
