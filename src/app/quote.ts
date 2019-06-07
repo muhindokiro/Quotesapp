3a0a735457ff60aa4b820f760c7ea1e448191cec
muhindokiro <49603911+muhindokiro@users.noreply.github.com>
2019-06-07 10:55:50 +0300

    add quote.ts

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -2,6 +2,5 @@ export class Quote {
     public showQuote:boolean
     constructor(public name:string,public quote:string, public author:string, public completeDate:Date){
         this.showQuote=false
-
     }
 }
